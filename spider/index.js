33b9b5073a63c1f785938805e6ba9128b36ac978
qing.xiao <qing.xiao@MIE-XIAOQING-D1.cd.ta-mp.com>
2016-03-04 14:08:44 +0800

    add entry url

--- spider/index.js
+++ spider/index.js
@@ -3,10 +3,20 @@
  */
 var request = require('superagent');
 var conf = require('./config.js');
+var login = require('./lib/login.js');
+var entry = require('./lib/entry.js');
 
+login()
+    .then(entry)
+    .then(function(entryUrl){
+        console.log(entryUrl)
+    });
+
+
+/*
 request
     .get('https://www.zhihu.com/people/xiao-yan-jing-43')
     .end(function(err, res){
         // Do something
         console.log(res);
-    });
+    });*/
